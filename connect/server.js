98815d63b3e824fcb0b0b49d248bdbe7e4babe3e
stanislavys <stanislavys@gmail.com>
2015-04-26 01:16:57 +0300

    Implement hello world, logger and trivial admin console with authentication

---
+++ connect/server.js
@@ -0,0 +1,61 @@
+var connect = require('connect');
+
+function logger(req, res, next) {
+	console.log('%s %s', req.method, req.url);
+	next();
+}
+
+function helloWorld(req, res) {
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('hello world');
+}
+
+function authenticateUser(user, pass, cb) {
+	if (user.indexOf('user') == 0) {
+		cb();
+	} else {
+		cb(new Error('Wrong username or password.'));
+	}
+}
+
+function restrict(req, res, next) {
+	var authorization  = req.headers.authorization;
+	if (!authorization) {
+		return next(new Error('Unauthorized'));
+	}
+	
+	var parts = authorization.split(' ');
+	var scheme = parts[0];
+	var auth = new Buffer(parts[1], 'base64').toString().split(':');
+	var user = auth[0];
+	var password = auth[1];
+	
+	authenticateUser(user, password, function(err) {
+		if (err) {
+			return next(err);
+		}
+		
+		next();
+	});
+}
+
+function adminConsole(req, res, next) {
+	switch (req.url) {
+		case '/': {
+			res.end('try /users');
+			break;
+		}
+		case '/users': {
+			res.setHeader('Content-Type', 'application/json');
+			res.end(JSON.stringify(['user1', 'alice', 'bob']));
+			break;
+		}
+	}
+}
+
+connect()
+	.use(logger)
+	.use('/admin', restrict)
+	.use('/admin', adminConsole)
+	.use(helloWorld)
+	.listen(3000);
